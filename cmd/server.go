87b5c76378aa8a8b0aaa732fb5f0eb83676ef62c
Jacke1012 <jacob.jonasson2003@gmail.com>
2025-08-14 20:47:04 +0200

    first commit

---
+++ cmd/server.go
@@ -0,0 +1,139 @@
+// Copyright © 2018 Enrico Stahn <enrico.stahn@gmail.com>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/hipages/php-fpm_exporter/phpfpm"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/cobra"
+)
+
+// Configuration variables
+var (
+	listeningAddress string
+	metricsEndpoint  string
+	scrapeURIs       []string
+	fixProcessCount  bool
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Infof("Starting server on %v with path %v", listeningAddress, metricsEndpoint)
+
+		pm := phpfpm.PoolManager{}
+
+		for _, uri := range scrapeURIs {
+			pm.Add(uri)
+		}
+
+		exporter := phpfpm.NewExporter(pm)
+
+		if fixProcessCount {
+			log.Info("Idle/Active/Total Processes will be calculated by php-fpm_exporter.")
+			exporter.CountProcessState = true
+		}
+
+		prometheus.MustRegister(exporter)
+
+		srv := &http.Server{
+			Addr: listeningAddress,
+			// Good practice to set timeouts to avoid Slowloris attacks.
+			WriteTimeout: time.Second * 15,
+			ReadTimeout:  time.Second * 15,
+			IdleTimeout:  time.Second * 60,
+		}
+
+		http.Handle(metricsEndpoint, promhttp.Handler())
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			_, err := w.Write([]byte(`<html>
+			 <head><title>php-fpm_exporter</title></head>
+			 <body>
+			 <h1>php-fpm_exporter</h1>
+			 <p><a href='` + metricsEndpoint + `'>Metrics</a></p>
+			 </body>
+			 </html>`))
+
+			if err != nil {
+				log.Error()
+			}
+		})
+
+		// Run our server in a goroutine so that it doesn't block.
+		go func() {
+			if err := srv.ListenAndServe(); err != nil {
+				log.Error(err)
+			}
+		}()
+
+		c := make(chan os.Signal, 1)
+		// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C) or SIGTERM
+		// SIGKILL, SIGQUIT will not be caught.
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+		// Block until we receive our signal.
+		<-c
+
+		// Create a deadline to wait for.
+		wait := time.Second * 10
+		ctx, cancel := context.WithTimeout(context.Background(), wait)
+		defer cancel()
+		// Doesn't block if no connections, but will otherwise wait
+		// until the timeout deadline.
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Fatal("Error during shutdown", err)
+		}
+		// Optionally, you could run srv.Shutdown in a goroutine and block on
+		// <-ctx.Done() if your application should wait for other services
+		// to finalize based on context cancellation.
+		log.Info("Shutting down")
+		os.Exit(0)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(serverCmd)
+
+	serverCmd.Flags().StringVar(&listeningAddress, "web.listen-address", ":9253", "Address on which to expose metrics and web interface.")
+	serverCmd.Flags().StringVar(&metricsEndpoint, "web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+	serverCmd.Flags().StringSliceVar(&scrapeURIs, "phpfpm.scrape-uri", []string{"tcp://127.0.0.1:9000/status"}, "FastCGI address, e.g. unix:///tmp/php.sock;/status or tcp://127.0.0.1:9000/status")
+	serverCmd.Flags().BoolVar(&fixProcessCount, "phpfpm.fix-process-count", false, "Enable to calculate process numbers via php-fpm_exporter since PHP-FPM sporadically reports wrong active/idle/total process numbers.")
+
+	// Workaround since vipers BindEnv is currently not working as expected (see https://github.com/spf13/viper/issues/461)
+
+	envs := map[string]string{
+		"PHP_FPM_WEB_LISTEN_ADDRESS": "web.listen-address",
+		"PHP_FPM_WEB_TELEMETRY_PATH": "web.telemetry-path",
+		"PHP_FPM_SCRAPE_URI":         "phpfpm.scrape-uri",
+		"PHP_FPM_FIX_PROCESS_COUNT":  "phpfpm.fix-process-count",
+	}
+
+	mapEnvVars(envs, serverCmd)
+}
---
+++ phpfpm/phpfpm_test.go
@@ -0,0 +1,134 @@
+// Copyright © 2018 Enrico Stahn <enrico.stahn@gmail.com>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package phpfpm
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCountProcessState(t *testing.T) {
+	processes := []PoolProcess{
+		{State: PoolProcessRequestIdle},
+		{State: PoolProcessRequestRunning},
+		{State: PoolProcessRequestReadingHeaders},
+		{State: PoolProcessRequestInfo},
+		{State: PoolProcessRequestFinishing},
+		{State: PoolProcessRequestEnding},
+	}
+
+	active, idle, total := CountProcessState(processes)
+
+	assert.Equal(t, int64(2), active, "active processes")
+	assert.Equal(t, int64(1), idle, "idle processes")
+	assert.Equal(t, int64(3), total, "total processes")
+}
+
+// https://github.com/hipages/php-fpm_exporter/issues/10
+func TestCannotUnmarshalNumberIssue10(t *testing.T) {
+	pool := Pool{}
+	content := []byte(`{
+	   "pool":"www",
+	   "process manager":"dynamic",
+	   "start time":1519474655,
+	   "start since":302035,
+	   "accepted conn":44144,
+	   "listen queue":0,
+	   "max listen queue":1,
+	   "listen queue len":128,
+	   "idle processes":1,
+	   "active processes":1,
+	   "total processes":2,
+	   "max active processes":2,
+	   "max children reached":0,
+	   "slow requests":0,
+	   "processes":[
+		  {
+			 "pid":23,
+			 "state":"Idle",
+			 "start time":1519474655,
+			 "start since":302035,
+			 "requests":22071,
+			 "request duration":295,
+			 "request method":"GET",
+			 "request uri":"/status?json&full",
+			 "content length":0,
+			 "user":"-",
+			 "script":"-",
+			 "last request cpu":0.00,
+			 "last request memory":2097152
+		  },
+		  {
+			 "pid":24,
+			 "state":"Running",
+			 "start time":1519474655,
+			 "start since":302035,
+			 "requests":22073,
+			 "request duration":18446744073709550774,
+			 "request method":"GET",
+			 "request uri":"/status?json&full",
+			 "content length":0,
+			 "user":"-",
+			 "script":"-",
+			 "last request cpu":0.00,
+			 "last request memory":0
+		  }
+	   ]
+    }`)
+
+	err := json.Unmarshal(content, &pool)
+
+	assert.Nil(t, err, "successfully unmarshal on invalid 'request duration'")
+	assert.Equal(t, int(pool.Processes[0].RequestDuration), 295, "request duration set to 0 because it couldn't be deserialized")
+	assert.Equal(t, int(pool.Processes[1].RequestDuration), 0, "request duration set to 0 because it couldn't be deserialized")
+}
+
+// https://github.com/hipages/php-fpm_exporter/issues/24
+func TestInvalidCharacterIssue24(t *testing.T) {
+	// todo: Implement fcgi client dependency injection to allow testing of Pool.Update
+}
+
+func TestJsonResponseFixer(t *testing.T) {
+	pool := Pool{}
+	content := []byte(`{"pool":"www","process manager":"dynamic","start time":1528367006,"start since":15073840,"accepted conn":1577112,"listen queue":0,"max listen queue":0,"listen queue len":0,"idle processes":16,"active processes":1,"total processes":17,"max active processes":15,"max children reached":0,"slow requests":0, "processes":[{"pid":15873,"state":"Idle","start time":1543354120,"start since":86726,"requests":853,"request duration":5721,"request method":"GET","request uri":"/vbseo.php?ALTERNATE_TEMPLATES=|%20echo%20"Content-Type:%20text%2Fhtml"%3Becho%20""%20%3B%20id%00","content length":0,"user":"-","script":"/www/forum.example.com/vbseo.php","last request cpu":349.59,"last request memory":786432},{"pid":123,"state":"Idle","start time":1543354120,"start since":86726,"requests":853,"request duration":5721,"request method":"GET","request uri":"123/vbseo.php?ALTERNATE_TEMPLATES=|%20echo%20"Content-Type:%20text%2Fhtml"%3Becho%20""%20%3B%20id%00","content length":0,"user":"-","script":"/www/forum.example.com/vbseo.php","last request cpu":349.59,"last request memory":786432}]}`)
+
+	content = JSONResponseFixer(content)
+
+	err := json.Unmarshal(content, &pool)
+
+	assert.Nil(t, err, "successfully unmarshal on invalid 'request uri'")
+	assert.Equal(t, pool.Processes[0].RequestURI, `/vbseo.php?ALTERNATE_TEMPLATES=|%20echo%20"Content-Type:%20text%2Fhtml"%3Becho%20""%20%3B%20id%00`, "request uri couldn't be deserialized")
+	assert.Equal(t, pool.Processes[1].RequestURI, `123/vbseo.php?ALTERNATE_TEMPLATES=|%20echo%20"Content-Type:%20text%2Fhtml"%3Becho%20""%20%3B%20id%00`, "request uri couldn't be deserialized")
+}
+
+func TestParseURL(t *testing.T) {
+	var uris = []struct {
+		in  string
+		out []string
+		err error
+	}{
+		{"tcp://127.0.0.1:9000/status", []string{"tcp", "127.0.0.1:9000", "/status"}, nil},
+		{"tcp://127.0.0.1", []string{"tcp", "127.0.0.1", ""}, nil},
+		{"unix:///tmp/php.sock;/status", []string{"unix", "/tmp/php.sock", "/status"}, nil},
+		{"unix:///tmp/php.sock", []string{"unix", "/tmp/php.sock", ""}, nil},
+	}
+
+	for _, u := range uris {
+		scheme, address, path, err := parseURL(u.in)
+		assert.Equal(t, u.err, err)
+		assert.Equal(t, u.out, []string{scheme, address, path})
+	}
+}
